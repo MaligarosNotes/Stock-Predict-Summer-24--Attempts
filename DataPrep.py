0778650460d1a9b068f57e12b29854e215c681cc
MaligarosNotes <170369572+MaligarosNotes@users.noreply.github.com>
2024-07-06 16:12:37 -0400

    messed around with the dataprep file, and 4thstock

--- DataPrep.py
+++ DataPrep.py
@@ -19,10 +19,14 @@ def TickerIntoTensor(ticker='aapl',daysago=100,daysforward=1,split_percentage=0.
     df = df.drop(columns=['Close'])
     df = df.drop(index=df.tail(daysforward).index)
     df = df.drop(index=df.head(daysago).index)
-    dfstddev = df.iloc[:,:daysago+1].std(axis=1)
+    dfstddev = pd.DataFrame()
+    dfstddev['daysago# std'] = df.iloc[:,:daysago+1].std(axis=1)
+    dfstddev['half of daysago# std'] = df.iloc[:,daysago//2:daysago+1].std(axis=1)
+    dfstddev['third of daysago# std'] = df.iloc[:,daysago-(daysago//3):daysago+1].std(axis=1)
+    dfstddev['quarter of daysago# std'] = df.iloc[:,daysago-(daysago//4):daysago+1].std(axis=1)
+    dfstddev['ten days from daysago# std'] = df.iloc[:,daysago-10:daysago+1].std(axis=1)
     sc = MinMaxScaler(feature_range=(0,1))
-    stddev_scaled = sc.fit_transform(dfstddev.values.reshape(-1,1))
-    print(stddev_scaled)
+    stddev_scaled = sc.fit_transform(dfstddev)
     df_scaled = sc.fit_transform(df)
     X,Y = df_scaled[:,:daysago+1],df_scaled[:,-daysforward:]
     if split_percentage <= 1 and split_percentage >= 0:
@@ -31,7 +35,7 @@ def TickerIntoTensor(ticker='aapl',daysago=100,daysforward=1,split_percentage=0.
         split = split_percentage
     X_train, X_test, Y_train, Y_test = X[:split], X[split:], Y[:split], Y[split:]
     X_train, X_test = np.append(stddev_scaled[:split],X_train,axis=1), np.append(stddev_scaled[split:],X_test,axis=1)
-    X_train, X_test, Y_train, Y_test = X_train.reshape((-1,daysago+1+1,1)), X_test.reshape((-1,daysago+1+1,1)), Y_train.reshape((-1,daysforward)), Y_test.reshape((-1,daysforward))
+    X_train, X_test, Y_train, Y_test = X_train.reshape((-1,daysago+1+dfstddev.shape[1],1)), X_test.reshape((-1,daysago+1+dfstddev.shape[1],1)), Y_train.reshape((-1,daysforward)), Y_test.reshape((-1,daysforward))
 
     return X_train, X_test, Y_train, Y_test
-TickerIntoTensor('aapl',100,1,-1)
+print(TickerIntoTensor('aapl',100,1,0.9)[0].shape)
