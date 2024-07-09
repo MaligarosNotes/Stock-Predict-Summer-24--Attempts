1aa5b9674c2bae2cfd025f6e4b362428171e2f0f
MaligarosNotes <170369572+MaligarosNotes@users.noreply.github.com>
2024-07-09 17:52:37 -0400

    shelved 4thstockpredictor, onto 5th, with inspo from digits recognition, % probability thing

--- 4th attempt total/DataPrep.py
+++ 4th attempt total/DataPrep.py
@@ -6,8 +6,31 @@ import keras
 import os
 import datetime 
 from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.layers import TimeDistributed
+from keras.layers import Activation
+from keras import optimizers
+from keras.models import Model
+from keras.layers import Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.optimizers import Adam
 
-def TickerIntoTensor(ticker='aapl',daysago=100,daysforward=1,split_percentage=0.9):
+def addStdDev(df,IV=1,daysago=100):
+    dfstddev = pd.DataFrame()
+    for i in range(1,IV):
+        dfstddev['1/{} of daysago# std'.format(i)] = df.iloc[:,daysago-(daysago//i):daysago+1].std(axis=1)
+    sc = MinMaxScaler(feature_range=(0,1))
+    stddev_scaled = sc.fit_transform(dfstddev)
+    return stddev_scaled, dfstddev
+
+def TickerIntoTensor(ticker='aapl',daysago=100,daysforward=1,split_percentage=0.9,IV=1):
     df = yf.Ticker(ticker.upper())
     df = df.history(period='max')
     if df.shape[1] == 7:
@@ -19,14 +42,19 @@ def TickerIntoTensor(ticker='aapl',daysago=100,daysforward=1,split_percentage=0.
     df = df.drop(columns=['Close'])
     df = df.drop(index=df.tail(daysforward).index)
     df = df.drop(index=df.head(daysago).index)
-    dfstddev = pd.DataFrame()
-    dfstddev['daysago# std'] = df.iloc[:,:daysago+1].std(axis=1)
-    dfstddev['half of daysago# std'] = df.iloc[:,daysago//2:daysago+1].std(axis=1)
-    dfstddev['third of daysago# std'] = df.iloc[:,daysago-(daysago//3):daysago+1].std(axis=1)
-    dfstddev['quarter of daysago# std'] = df.iloc[:,daysago-(daysago//4):daysago+1].std(axis=1)
-    dfstddev['ten days from daysago# std'] = df.iloc[:,daysago-10:daysago+1].std(axis=1)
+
+
+
+    if IV == 0:
+        literally_useless = 0
+    else:
+        tempSTDDEV = addStdDev(df,IV,daysago)
+        stddev_scaled = tempSTDDEV[0]
+        dfstddev = tempSTDDEV[1]
+
+
+
     sc = MinMaxScaler(feature_range=(0,1))
-    stddev_scaled = sc.fit_transform(dfstddev)
     df_scaled = sc.fit_transform(df)
     X,Y = df_scaled[:,:daysago+1],df_scaled[:,-daysforward:]
     if split_percentage <= 1 and split_percentage >= 0:
@@ -38,4 +66,4 @@ def TickerIntoTensor(ticker='aapl',daysago=100,daysforward=1,split_percentage=0.
     X_train, X_test, Y_train, Y_test = X_train.reshape((-1,daysago+1+dfstddev.shape[1],1)), X_test.reshape((-1,daysago+1+dfstddev.shape[1],1)), Y_train.reshape((-1,daysforward)), Y_test.reshape((-1,daysforward))
 
     return X_train, X_test, Y_train, Y_test
-print(TickerIntoTensor('aapl',100,1,0.9)[0].shape)
+print(TickerIntoTensor('aapl',200,1,0.9,20)[0].shape)
